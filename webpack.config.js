9cbe5eb97f44e05ffcac2cc1ac1b2439e215380b
Thach Nguyen <thachnn80@gmail.com>
2023-02-04 17:26:55 +0700

    Optimize webpack config-file for bundling v1.x using the latest bundled `webpack` v5

--- webpack.config.js
+++ webpack.config.js
@@ -8,132 +8,151 @@ const [nodeVersion] = /\d[\d.]*/.exec(pkg.engines.node);
 module.exports = {
   mode: 'production',
   entry: {
-    _collections: { import: './node_modules/svgo/plugins/_collections', library: { type: 'commonjs' } },
-    plugins: { import: './node_modules/svgo/plugins/plugins', library: { type: 'commonjs' } },
-    'vendor/css-select': { import: './node_modules/css-select/lib/index', library: { type: 'commonjs' } },
-    'vendor/css-tree': { import: './node_modules/css-tree/lib/index', library: { type: 'commonjs2' } },
-    'vendor/csso': { import: './node_modules/csso/lib/index', library: { type: 'commonjs2' } },
-    index: { import: './node_modules/svgo/lib/svgo-node', library: { type: 'commonjs' } },
-    cli: { import: './node_modules/svgo/bin/svgo' },
+    // jsAPI: './node_modules/svgo/lib/svgo/jsAPI',
+    config: './node_modules/svgo/lib/svgo/config',
+    'vendor/css-select': './node_modules/css-select/index',
+    'vendor/css-tree': './node_modules/css-tree/lib/syntax/index',
+    'vendor/csso': './node_modules/csso/lib/index',
+    'vendor/coa': './node_modules/coa/lib/index',
+    'vendor/chalk': './node_modules/chalk/index',
+    'vendor/js-yaml': './node_modules/js-yaml/lib/js-yaml/loader',
+    // 'css-tools': './node_modules/svgo/lib/css-tools',
+    index: './node_modules/svgo/lib/svgo.js',
+    cli: {
+      import: './node_modules/svgo/bin/svgo',
+      library: { type: 'assign', name: '__webpack_exports__' },
+    },
   },
-  // output: { path: __dirname },
+  output: { libraryTarget: 'commonjs2' },
   context: __dirname,
   target: `node${nodeVersion}`,
   node: { __filename: false, __dirname: false },
   externals: {
-    '../plugins/_collections.js': 'commonjs ./_collections',
-    './_collections.js': 'commonjs ./_collections',
-    './_collections': 'commonjs ./_collections',
-    '../../plugins/plugins.js': 'commonjs ./plugins',
-    'css-select': 'commonjs ./vendor/css-select',
+    // './svgo/jsAPI.js': 'commonjs2 ./jsAPI',
+    // './jsAPI.js': 'commonjs2 ./jsAPI',
+    // '../lib/svgo/jsAPI': 'commonjs2 ./jsAPI',
+    './svgo/config.js': 'commonjs2 ./config',
+    '../lib/svgo.js': 'commonjs2 ./index',
+    'css-select': 'commonjs2 ./vendor/css-select',
     'css-tree': 'commonjs2 ./vendor/css-tree',
     csso: 'commonjs2 ./vendor/csso',
-    '../svgo-node.js': 'commonjs ./index',
-    originalRequire: 'commonjs2 ./originalRequire',
+    coa: 'commonjs2 ./vendor/coa',
+    chalk: 'commonjs2 ./vendor/chalk',
+    'js-yaml': 'commonjs2 ./vendor/js-yaml',
+    // '../css-tools': 'commonjs2 ./css-tools',
+    // '../lib/css-tools': 'commonjs2 ./css-tools',
+    '../svgo.js': 'commonjs2 ./index',
   },
   cache: { type: 'filesystem' },
   stats: { modulesSpace: Infinity, nestedModules: true, nestedModulesSpace: Infinity },
   module: {
     rules: [
       {
-        // transpile ES6-8 into ES5
-        test: /\.m?js$/i,
-        exclude:
-          /node_modules.(@trysound|boolbase|csso?|dom(elementtype|handler|utils)?|entities|mdn|nth|source|stable)\b/i,
-        loader: 'babel-loader',
+        test: /node_modules[\\/]svgo.package\.json$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: /"keywords":[\s\S]*/, replace: `"engines": ${JSON.stringify(pkg.engines)}\n}` },
+      },
+      // apply patches
+      {
+        test: /node_modules[\\/]svgo\b.plugins.removeViewBox\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: /^(exports\.active *=) *true\b/m, replace: '$1 false' },
+      },
+      {
+        test: /node_modules[\\/]svgo\b.plugins.convertPathData\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: /^\s*(curveSmoothShorthands|noSpaceAfterFlags): *true\b/gm, replace: '$1: false' },
+      },
+      {
+        test: /node_modules[\\/](svgo\b.lib\b.svgo.config|coa\b.lib.(cmd|index))\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: / require(\(\w+|$)/gm, replace: ' __non_webpack_require__$1' }, // TODO require:
+      },
+      {
+        test: /node_modules[\\/]svgo\b.lib\b.svgo.config\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: /\b(__dirname) *\+ *'[^']*(\.\.\/\.svgo\.yml)'/, replace: "PATH.resolve($1, '$2')" },
+      },
+      // patches for Node.js
+      {
+        test: /node_modules[\\/]csso\b.lib\b.clean.[ARW]\w*\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: /\{\s*(\w+)\s*\}( *= *require\(['"][^()]*\))/g, replace: '$1$2.$1' },
+      },
+      {
+        test: /node_modules[\\/]svgo\b.lib\b.svgo.css-class-list\.js$/i,
+        loader: 'webpack/lib/replace-loader',
         options: {
-          cacheDirectory: true,
-          presets: [
-            ['@babel/preset-env', { targets: { node: nodeVersion }, modules: false }], // esmodules
+          multiple: [
+            { search: /^.*= *require\('o(bject\.values)'\);?\s*if *\(!O\1\) *\{[^{}]*\}/m, replace: '/*$&*/' },
+            { search: /\bObject\.values(\(arguments\))/g, replace: 'Array.prototype.slice.call$1' },
           ],
         },
       },
       {
-        test: /node_modules.svgo.package\.json$/i,
-        loader: 'string-replace-loader',
+        test: /node_modules[\\/]svgo\b.lib\b.svgo.coa\.js$/i,
+        loader: 'webpack/lib/replace-loader',
         options: {
           multiple: [
-            { search: /"packageManager":\s*"[^"]*",\s*/, replace: '' },
-            { search: /"keywords":[\s\S]*/, replace: `"engines": { "node": "${pkg.engines.node}" }\n}` },
-          ],
-        },
-      },
-      {
-        test: /node_modules.svgo.plugins.preset-default\.js$/i,
-        loader: 'string-replace-loader',
-        options: { search: /^(const)? *removeViewBox\b/gm, replace: '// $&' },
-      },
-      {
-        test: /node_modules.svgo.plugins.removeViewBox\.js$/i,
-        loader: 'string-replace-loader',
-        options: { search: /\b(exports\.active *= *)true\b/, replace: '$1false' },
-      },
-      {
-        test: /node_modules.svgo.plugins.convertPathData\.js$/i,
-        loader: 'string-replace-loader',
-        options: { search: /\b(curveSmoothShorthands|noSpaceAfterFlags): *true\b/g, replace: '$1: false' },
-      },
-      {
-        test: /node_modules.svgo.lib.svgo-node\.js$/i,
-        loader: 'string-replace-loader',
-        options: {
-          multiple: [
-            { search: ' import(path', replace: ' import(/* webpackIgnore: true */ path' },
-            { search: / require\(config/g, replace: ' require("originalRequire")(config' },
-            { search: ' isFile = async (file)', replace: ' isFile = (file)' },
-            { search: ' await fs.promises.stat(file)', replace: ' fs.statSync(file)' },
-            { search: /\bawait isFile\(/g, replace: 'isFile(' },
-          ],
-        },
-      },
-      {
-        test: /node_modules.svgo.lib.svgo.coa\.js$/i,
-        loader: 'string-replace-loader',
-        options: {
-          multiple: [
-            {
-              search: /^const regSVGFile =.*/m,
-              replace: `$&
-if (typeof fs.promises == 'undefined') {
-  const { promisify } = require('util');
-  fs.promises = ['readdir', 'readFile', 'writeFile'].reduce((obj, x) => (obj[x] = promisify(fs[x]), obj), {});
-}`,
-            },
-            {
-              search: /^ *fs\.mkdirSync\b.*\brecursive\b.*/m,
-              replace: " try { $& } catch (e) { if (e.code != 'EEXIST') throw e; }",
-            },
+            { search: /= *(require\('util)(\.promisify)('\))/, replace: '= $1$3$2 || $1$2$3' },
+            { search: /= *(promisify\(FS\.(\w+)\))/g, replace: '= FS.promises ? FS.promises.$2 : $1' },
           ],
         },
       },
       // optimize output
       {
-        test: /node_modules.dom-serializer.lib.index\.js$/i,
-        loader: 'string-replace-loader',
-        options: { search: ' = require("entities")', replace: ' = require("entities/lib/encode")' },
+        test: /node_modules[\\/]ansi-styles.index\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: {
+          search: /^Object\.defineProperty\(module, *'exports', *\{[^{}]*\bget: *(\w+)[^{}]*\}\)/m,
+          replace: 'module.exports = $1()',
+        },
       },
       {
-        test: /node_modules.entities.lib.encode\.js$/i,
-        loader: 'string-replace-loader',
+        test: /node_modules[\\/]js-yaml\b.lib\b.js-yaml.loader\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: {
+          search: /^(var DEFAULT_FULL_SCHEMA)( *= *require\('\.\/schema)\/default_full\b/m,
+          replace: '$1 = module.exports.FAILSAFE_SCHEMA$2/failsafe',
+        },
+      },
+      {
+        test: /node_modules[\\/]entities\b.lib.encode\.js$/i,
+        loader: 'webpack/lib/replace-loader',
         options: {
           multiple: [
-            { search: ' = exports.encodeXML = ', replace: ' =\n exports.encodeXML = ' },
-            { search: /^exports\.(?!encodeXML)\w* = /gm, replace: '// $&' },
-            { search: /^var (entities_json\w*|inverseHTML|htmlReplacer) = /gm, replace: '// $&' },
+            { search: /^exports\.(?!XML\b)\w+ *=/gm, replace: '//$&' },
+            { search: /^exports\.XML *=/m, replace: 'exports.encodeXML = $&' },
+            { search: /^var (inverseHTML|re_xmlChars) *=[^;]*;/gm, replace: '/*$&*/' },
           ],
         },
       },
+      {
+        test: /node_modules[\\/]sax\b.lib.sax\.js$/i, // only SAXParser
+        loader: 'webpack/lib/replace-loader',
+        options: {
+          multiple: [
+            { search: /^\s*sax\.(SAXStream|createStream) *=/gm, replace: '//$&' },
+            { search: /^\s*var Stream;?\n[\s\S]*(?=\n[ \t]*var CDATA *=)/m, replace: '/*\n$&\n*/' },
+            { search: /^\s*sax\.EVENTS *= *\[[^\[\]]*\];?/m, replace: '/*\n$&\n*/' },
+          ],
+        },
+      },
+      {
+        test: /node_modules[\\/]q\b.q\.js$/i,
+        loader: 'webpack/lib/replace-loader',
+        options: { search: /^(\(function\b.*\{\n)[\s\S]*( if \(typeof exports\b)/m, replace: '$1$2' },
+      },
     ],
+  },
+  resolve: {
+    alias: { entities$: __dirname + '/node_modules/entities/lib/encode.js' },
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        {
-          from: 'node_modules/svgo/*/*.ts',
-          to: '[name].d[ext]',
-          transform: (content) => String(content).replace(/^(export )?(type )/gm, '$1declare $2'),
-        },
-        { from: 'node_modules/svgo/LICENSE*', to: '../[name][ext]' },
+        { from: 'node_modules/svgo/{LICENSE*,.svgo.*}', to: '../[name][ext]' },
+        { from: 'node_modules/coa/lib/completion.sh', to: 'vendor/' },
       ],
     }),
     new BannerPlugin({
@@ -142,8 +161,8 @@ if (typeof fs.promises == 'undefined') {
       test: /\bcli\.js$/,
     }),
     new ReplaceCodePlugin([
-      { search: ' require("./originalRequire")', replace: ' require', test: /\bindex\.js$/ },
-      { search: ' require("./vendor/', replace: ' require("./', test: /\bvendor.[\w-]*\.js$/ },
+      { search: /\b(__webpack_exports__) = \1;?$/m, replace: '', test: /\bcli\.js$/ },
+      { search: /( require\("\.\/)vendor\//g, replace: '$1', test: /\bvendor[\\/][\w-]*\.js$/ },
     ]),
   ],
   optimization: {
@@ -152,7 +171,7 @@ if (typeof fs.promises == 'undefined') {
     minimizer: [
       new TerserPlugin({
         // parallel: true,
-        terserOptions: { mangle: false, output: { beautify: true, indent_level: 2 } },
+        terserOptions: { mangle: false, output: { beautify: true, indent_level: 2, comments: false } },
         extractComments: false,
       }),
     ],
